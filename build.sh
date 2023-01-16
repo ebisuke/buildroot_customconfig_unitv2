9a4d932978ce8631ca991c0d59a00f8ee59bd606
ebisuke <fungi@rect.sakura.ne.jp>
2023-01-16 13:19:34 +0900

    fix output

--- build.sh
+++ build.sh
@@ -52,6 +52,7 @@ make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
 
 echo "Building Buildroot"
 cd $BUILDROOT_UNITV2_PATH
+cp -f $INITIAL_PATH/configs/buildroot.config $BUILDROOT_PATH/.config
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
 
 echo "Creating output directory"
