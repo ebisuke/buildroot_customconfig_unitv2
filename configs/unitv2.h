bb269c7471c6d15e8058e93b00efa5282079cc81
ebisuke <fungi@rect.sakura.ne.jp>
2023-01-16 13:19:34 +0900

    fix output

---
+++ configs/unitv2.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ */
+
+#ifndef __UNITV2_CONFIG_H
+#define __UNITV2_CONFIG_H
+
+#include "mstarv7.h"
+
+//#define CONFIG_EXTRA_ENV_SETTINGS	"bootargs=console=ttyS0,115200 clk_ignore_unused usbcore.autosuspend=-1 "\
+//					"ubi.fm_autoconvert=1 ubi.mtd=2 ubi.block=0,rootfs root=/dev/ubiblock0_4\0"\
+//					"bootcmd_rescue=ubi part UBI; ubi readvol ${loadaddr} rescue; bootm ${loadaddr}\0"\
+//					"bootcmd=ubi part UBI; ubi readvol ${loadaddr} kernel; bootm ${loadaddr}\0"
+#endif
