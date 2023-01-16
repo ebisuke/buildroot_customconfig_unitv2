4e26f077f3d2a5f5cca2748963b0a0aa19596284
ebisuke <fungi@rect.sakura.ne.jp>
2023-01-16 22:35:23 +0900

    fix output

--- configs/unitv2.h
+++ configs/unitv2.h
@@ -8,8 +8,8 @@
 #include "mstarv7.h"
 
 
-#define CONFIG_EXTRA_ENV_SETTINGS	"bootargs=console=ttyS0,115200 boot=/dev/mmcblk0p1 rootwait rw rootfstype=ext4 init=/sbin/openrc-init\0"\
+#define CONFIG_EXTRA_ENV_SETTINGS	"bootargs=console=ttyS0,115200 root=/dev/mmcblk0p1 rootwait rw rootfstype=ext4 init=/sbin/openrc-init\0"\
 					"bootcmd_rescue=ubi part UBI; ubi readvol ${loadaddr} rescue; bootm ${loadaddr}\0"\
-					"bootcmd=ext4load mmc 0:1 ${loadaddr} /boot/gentoo-kernel.img;bootm ${loadaddr}\0"
+					"bootcmd=ext4load mmc 0:1 0x20008000 /boot/zImage;ext4load mmc 0:1 0x20208000 /boot/dts/mstar-infinity2m-ssd202d-unitv2.dtb;bootz 0x20008000 - 0x20208000\0"
 
 #endif
