db502dc3b3c59645704a1abaf114d67117d1e331
ebisuke <fungi@rect.sakura.ne.jp>
2023-01-18 12:58:57 +0900

    fix typo

--- configs/unitv2.h
+++ configs/unitv2.h
@@ -8,7 +8,7 @@
 #include "mstarv7.h"
 
 
-#define CONFIG_EXTRA_ENV_SETTINGS	"bootargs=console=ttyS0,115200 root=/dev/mmcblk0p1 rootwait rw rootfstype=ext4 init=/sbin/init clk_ignore_unused usbcore.autosuspend=-1 mtdparts=nand0:1M@0xec0000(UBOOT),256k(ENV),-(DATA)\0"\
+#define CONFIG_EXTRA_ENV_SETTINGS	"bootargs=console=ttyS0,115200 root=/dev/mmcblk0p1 rootwait rw rootfstype=ext4 init=/sbin/init clk_ignore_unused usbcore.autosuspend=-1 mtdparts=nand0:1m@0xec0000(UBOOT),256k(ENV),-(DATA)\0"\
 					"bootcmd_rescue=ubi part UBI; ubi readvol ${loadaddr} rescue; bootm ${loadaddr}\0"\
 					"bootcmd=ext4load mmc 0:1 ${loadaddr} /boot/gentoo-kernel.img;bootm ${loadaddr}\0"
 
